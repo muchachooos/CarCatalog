f2202a4de3e681834a35c17b542d3f1e31c97aea
muchachooos <odnorogg@bk.ru>
2024-04-08 02:03:34 +0300

    Modify handler

--- server/handler/car.go
+++ server/handler/car.go
@@ -18,12 +18,54 @@ func (s *Server) AddCarsHandler(c *gin.Context) {
 
 	for i := range carsReq.RegNums {
 		if carsReq.RegNums[i] == "" {
-			c.JSON(http.StatusBadRequest, model.Err{Error: "Empty string"})
+			c.JSON(http.StatusBadRequest, model.Err{Error: "Empty string in the body"})
 			return
 		}
 	}
 
 	err = s.Storage.AddCars(carsReq)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.Err{Error: "Database error: " + err.Error()})
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func (s *Server) ModifyCarsHandler(c *gin.Context) {
+	regNum, ok := c.GetQuery("regNum")
+	if regNum == "" || !ok {
+		c.JSON(http.StatusBadRequest, model.Err{Error: "Number is missing"})
+		return
+	}
+
+	var car model.Car
+
+	err := parser.ParseBody(c, &car)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, model.Err{Error: "Parse body error: " + err.Error()})
+		return
+	}
+
+	car.RegNum = regNum
+
+	err = s.Storage.ModifyCars(car)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, model.Err{Error: "Database error: " + err.Error()})
+		return
+	}
+
+	c.Status(http.StatusOK)
+}
+
+func (s *Server) DeleteCarHandler(c *gin.Context) {
+	regNum, ok := c.GetQuery("regNum")
+	if regNum == "" || !ok {
+		c.JSON(http.StatusBadRequest, model.Err{Error: "Number is missing"})
+		return
+	}
+
+	err := s.Storage.DeleteCar(regNum)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, model.Err{Error: "Database error: " + err.Error()})
 		return
