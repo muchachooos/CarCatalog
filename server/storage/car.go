1bb160afba10dbee2111fc204f87211864dad399
muchachooos <odnorogg@bk.ru>
2024-04-08 02:03:44 +0300

    Modify storage

--- server/storage/car.go
+++ server/storage/car.go
@@ -2,6 +2,8 @@ package storage
 
 import (
 	"CarCatalog/model"
+	"database/sql"
+	"errors"
 )
 
 func (s *Storage) AddCars(cars model.AddCarsReq) error {
@@ -10,6 +12,64 @@ func (s *Storage) AddCars(cars model.AddCarsReq) error {
 		if err != nil {
 			return err
 		}
+	}
+
+	return nil
+}
+
+func (s *Storage) ModifyCars(car model.Car) error {
+	if car.Mark != nil {
+		_, err := s.DB.NamedExec("UPDATE cars SET mark = :mark WHERE regNum = :regNum", car)
+		if err != nil {
+			return err
+		}
+	}
+
+	if car.Model != nil {
+		_, err := s.DB.NamedExec("UPDATE cars SET model = :model WHERE regNum = :regNum", car)
+		if err != nil {
+			return err
+		}
+	}
+
+	if car.Year != nil {
+		_, err := s.DB.NamedExec("UPDATE cars SET year = :year WHERE regNum = :regNum", car)
+		if err != nil {
+			return err
+		}
+	}
+
+	if car.Owner != nil {
+		_, err := s.DB.NamedExec("UPDATE cars SET owner = :owner WHERE regNum = :regNum", car)
+		if err != nil {
+			return err
+		}
+	}
+
+	if car.NewNum != nil {
+		_, err := s.DB.NamedExec("UPDATE cars SET regNum = :newNum WHERE regNum = :regNum", car)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Storage) DeleteCar(regNum string) error {
+	var car string
+
+	err := s.DB.Get(&car, "SELECT regNum FROM cars WHERE regNum = $1", regNum)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return model.ErrCarNotFound
+		}
+		return err
+	}
+
+	_, err = s.DB.Exec(`DELETE FROM cars WHERE regNum = $1`, regNum)
+	if err != nil {
+		return err
 	}
 
 	return nil
