607e48f380d314bedaefe8f0e29bf1fc06714658
muchachooos <odnorogg@bk.ru>
2024-04-08 02:04:08 +0300

    Modify model

--- server/model/model.go
+++ server/model/model.go
@@ -1,5 +1,9 @@
 package model
 
+import "errors"
+
+var ErrCarNotFound = errors.New("car not found")
+
 type Err struct {
 	Error string `json:"error"`
 }
@@ -15,6 +19,16 @@ type DBConf struct {
 	DBName   string `json:"dataBaseName"`
 	Sslmode  string `json:"sslmode"`
 }
+
 type AddCarsReq struct {
 	RegNums []string `json:"regNums"`
+}
+
+type Car struct {
+	RegNum string  `db:"regNum" json:"regNum"`
+	NewNum *string `db:"newNum" json:"newNum"`
+	Mark   *string `db:"mark" json:"mark"`
+	Model  *string `db:"model" json:"model"`
+	Year   *int    `db:"year" json:"year"`
+	Owner  *string `db:"owner" json:"owner"`
 }
